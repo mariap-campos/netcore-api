a5825a51e2b63b851ff6f13ed00574552cf57b4d
Maria <mpfc.maria@gmail.com>
2022-05-26 15:51:55 -0300

    Adicionar arquivos de projeto.

---
+++ Api.Data/Context/ContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Data.Context
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<MyContext>
+    {
+        public MyContext CreateDbContext(string[] args)
+        {
+
+            //Usado para Crias as Migrações
+            var connectionString = "Server=localhost;Port=3306;Database=dbAPI;Uid=root;Pwd=mudar@123";
+            var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
+            optionsBuilder.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 11)));
+            return new MyContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ Api.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        private DateTime? _createdAt;
+
+        public DateTime? CreateAt
+        {
+            get { return _createdAt; }
+            set { _createdAt = value == null ? DateTime.UtcNow : value; }
+        }
+
+        public DateTime? UpdateAt { get; set; }
+
+
+    }
+
+}
+
